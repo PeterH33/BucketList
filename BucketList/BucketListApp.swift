1d4a1cf4762d838fe824bcc63423923a7a31bdab
Peter Hartnett <peter.hartnett@gmail.com>
2022-04-16 16:56:30 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Peter Hartnett on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
