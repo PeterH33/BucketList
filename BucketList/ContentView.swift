087630c8c5c9faaf32982fb0470f591795070e52
Peter Hartnett <peter.hartnett@gmail.com>
2022-04-18 14:41:35 -0400

    Day two of the project, just an introduction to Map Kit and local authentication. Useful things to know

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,25 +7,58 @@
 
 import SwiftUI
 import MapKit
+import LocalAuthentication
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
     
+    @State private var isUnlocked = false
     
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
     var body: some View {
-        Text("Hello World")
-            .onTapGesture {
-                let str = "Test Message"
-                
-                let fileName = "message.txt"
-                do {
-                    try FileManager.default.encode(str, toFile: fileName)
-                    let input = try FileManager.default.decode(fileName, dataType: "String")
-                    print("Here is the file decoded \(input ?? "ERROR FAILURE")")
-                } catch {
-                    print(error.localizedDescription)
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
                 }
             }
+        } else {
+            // no biometrics
+        }
     }
 }
 
